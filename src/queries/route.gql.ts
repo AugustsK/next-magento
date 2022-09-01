c06067c20f35fd481163835a53a45ad1fc0c08e8
AugustsK <augusts.karklins@gmail.com>
2022-09-01 19:40:19 +0300

    move from fragment to route

--- src/queries/route.gql.ts
+++ src/queries/route.gql.ts
@@ -1,7 +1,5 @@
 import { gql } from '@apollo/client';
 
-import { ConfigurableProductFragment, SimpleProductFragment } from '@/queries/fragments/product.gql';
-
 export const getRoute = gql`
     query getRoute($url: String!, $currentPage: Int, $pageSize: Int, $productSort: ProductAttributeSortInput) {
         route(url: $url) {
@@ -48,13 +46,151 @@ export const getRoute = gql`
                 meta_keywords
             }
             ... on SimpleProduct {
-                ...SimpleProductFragment
+                uid
+                name
+                sku
+                canonical_url
+                short_description {
+                    html
+                }
+                media_gallery {
+                    disabled
+                    label
+                    position
+                    url
+                    ... on ProductVideo {
+                        video_content {
+                            media_type
+                            video_description
+                            video_metadata
+                            video_provider
+                            video_title
+                            video_url
+                        }
+                    }
+                }
+                meta_description
+                meta_keyword
+                meta_title
+                price_range {
+                    minimum_price {
+                        discount {
+                            amount_off
+                            percent_off
+                        }
+                        final_price {
+                            currency
+                            value
+                        }
+                        regular_price {
+                            currency
+                            value
+                        }
+                    }
+                    maximum_price {
+                        discount {
+                            amount_off
+                            percent_off
+                        }
+                        final_price {
+                            currency
+                            value
+                        }
+                        regular_price {
+                            currency
+                            value
+                        }
+                    }
+                }
+                short_description {
+                    html
+                }
+                special_to_date
+                stock_status
             }
             ... on ConfigurableProduct {
-                ...ConfigurableProductFragment
+                uid
+                name
+                sku
+                canonical_url
+                short_description {
+                    html
+                }
+                media_gallery {
+                    disabled
+                    label
+                    position
+                    url
+                    ... on ProductVideo {
+                        video_content {
+                            media_type
+                            video_description
+                            video_metadata
+                            video_provider
+                            video_title
+                            video_url
+                        }
+                    }
+                }
+                meta_description
+                meta_keyword
+                meta_title
+                price_range {
+                    minimum_price {
+                        discount {
+                            amount_off
+                            percent_off
+                        }
+                        final_price {
+                            currency
+                            value
+                        }
+                        regular_price {
+                            currency
+                            value
+                        }
+                    }
+                    maximum_price {
+                        discount {
+                            amount_off
+                            percent_off
+                        }
+                        final_price {
+                            currency
+                            value
+                        }
+                        regular_price {
+                            currency
+                            value
+                        }
+                    }
+                }
+                short_description {
+                    html
+                }
+                special_to_date
+                stock_status
+                configurable_options {
+                    attribute_code
+                    attribute_uid
+                    label
+                    position
+                    uid
+                    use_default
+                    values {
+                        default_label
+                        use_default_value
+                        store_label
+                        uid
+                        swatch_data {
+                            value
+                            ... on ImageSwatchData {
+                                thumbnail
+                            }
+                        }
+                    }
+                }
             }
         }
     }
-    ${ConfigurableProductFragment}
-    ${SimpleProductFragment}
 `;
